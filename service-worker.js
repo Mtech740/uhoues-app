47657b2f3f77cf1f9218d1631bac9c6e0434f561
Mtech740 <mumbasampa3@gmail.com>
2026-01-08 01:29:55 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,37 @@
-const CACHE_NAME = 'uhoues-v1';
+// Uhoues Service Worker
+const CACHE_NAME = 'uhoues-v1.0';
 const urlsToCache = [
   '/',
   'https://uhoues.streamlit.app/',
-  'https://img.icons8.com/color/192/000000/home--v1.png'
+  'https://img.icons8.com/color/192/000000/home--v1.png',
+  'https://img.icons8.com/color/512/000000/home--v1.png'
 ];
 
 self.addEventListener('install', event => {
+  console.log('Uhoues Service Worker installing...');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Uhoues cache opened');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          console.log('Uhoues serving from cache:', event.request.url);
+          return response;
+        }
+        console.log('Uhoues fetching from network:', event.request.url);
+        return fetch(event.request);
+      })
   );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Uhoues Service Worker activated');
 });
